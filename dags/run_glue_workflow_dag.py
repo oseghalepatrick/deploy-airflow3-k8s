961894c99c2f9828dea2e341a595482b849090b5
Patrick Oseghale <oseghale_patrick@yahoo.com>
2026-03-31 08:01:03 +0100

    add transformation

--- dags/run_glue_workflow_dag.py
+++ dags/run_glue_workflow_dag.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-
-from airflow.sdk import dag, task
-from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
-
-from produce_data import posts_asset, users_asset
 from pathlib import Path
+
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.sdk import dag, task
+from produce_data import posts_asset, users_asset
 
 DAG_DIR = Path(__file__).resolve().parent
 
@@ -14,7 +13,7 @@ IAM_ROLE = "GlueNotebookTutorialRole"
 
 S3_BUCKET = "stackexchange-data-platform-joy"
 
-LOCAL_GLUE_SCRIPT = str(DAG_DIR / "scripts" )
+LOCAL_GLUE_SCRIPT = str(DAG_DIR / "scripts")
 S3_GLUE_SCRIPT_KEY = "scripts"
 S3_BRONZE_POSTS_GLUE_SCRIPT_KEY = "scripts/bronze_posts.py"
 S3_BRONZE_USERS_GLUE_SCRIPT_KEY = "scripts/bronze_users.py"
@@ -28,6 +27,7 @@ GLUE_BRONZE_POSTS_JOB_NAME = "bronze-posts-xml-to-iceberg"
 GLUE_SILVER_POSTS_JOB_NAME = "silver-posts-to-iceberg"
 GLUE_BRONZE_USERS_JOB_NAME = "bronze-users-xml-to-iceberg"
 
+
 @dag(
     dag_id="run_aws_glue_job",
     start_date=datetime(2026, 3, 30),
@@ -38,24 +38,49 @@ GLUE_BRONZE_USERS_JOB_NAME = "bronze-users-xml-to-iceberg"
 def upload_and_run_aws_glue_job():
 
     @task
-    def upload_directory_to_s3(s3_bucket, local_directory, s3_key_prefix):
+    def upload_directory_to_s3(
+        s3_bucket, local_directory, s3_key_prefix, aws_conn_id="aws_default"
+    ):
         """
-        Upload all files from the local directory to S3 in one task.
+        Upload all files from the local directory to S3 in one task using AWS connection from Airflow.
         """
-        import os
-        import boto3
 
-        s3 = boto3.client('s3')
+        import os
+
+        import boto3
+        from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
+
+        # Use Airflow's AWS connection to get the credentials
+        aws_hook = AwsBaseHook(aws_conn_id=aws_conn_id)
+        credentials = aws_hook.get_credentials()
+
+        # Create a boto3 client using the credentials from Airflow
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=credentials.access_key,
+            aws_secret_access_key=credentials.secret_key,
+            aws_session_token=credentials.session_token,
+        )
+
+        # Iterate over the local directory and upload files to S3
         for root, dirs, files in os.walk(local_directory):
             for file in files:
                 local_file_path = os.path.join(root, file)
-                s3_key = os.path.join(s3_key_prefix, os.path.relpath(local_file_path, local_directory))
-                
+                s3_key = os.path.join(
+                    s3_key_prefix, os.path.relpath(local_file_path, local_directory)
+                )
+
                 # Upload each file to S3
                 s3.upload_file(local_file_path, s3_bucket, s3_key)
                 print(f"Uploaded {local_file_path} to s3://{s3_bucket}/{s3_key}")
 
-    upload_task = upload_directory_to_s3(S3_BUCKET, LOCAL_GLUE_SCRIPT, S3_GLUE_SCRIPT_KEY)
+    # Usage in your DAG
+    upload_task = upload_directory_to_s3(
+        s3_bucket=S3_BUCKET,
+        local_directory=LOCAL_GLUE_SCRIPT,
+        s3_key_prefix=S3_GLUE_SCRIPT_KEY,
+        aws_conn_id=AWS_CONN_ID
+    )
 
     run_bronze_posts_glue_job = GlueJobOperator(
         task_id="run_bronze_posts_glue_job",
@@ -73,7 +98,6 @@ def upload_and_run_aws_glue_job():
             "--source_key": "raw/posts/Posts.xml",
             "--catalog_database": "stackexchange_data_platform_db",
             "--catalog_table": "raw_posts",
-
             # Iceberg / Spark config from your notebook, translated to Glue job args
             "--datalake-formats": "iceberg",
             "--conf": (
@@ -92,9 +116,7 @@ def upload_and_run_aws_glue_job():
             "GlueVersion": "5.0",
             "WorkerType": "G.1X",
             "NumberOfWorkers": 2,
-            "ExecutionProperty": {
-                "MaxConcurrentRuns": 1
-            },
+            "ExecutionProperty": {"MaxConcurrentRuns": 1},
             "Command": {
                 "Name": "glueetl",
                 "ScriptLocation": S3_BRONZE_POSTS_GLUE_SCRIPT_PATH,
@@ -132,9 +154,7 @@ def upload_and_run_aws_glue_job():
             "GlueVersion": "5.0",
             "WorkerType": "G.1X",
             "NumberOfWorkers": 2,
-            "ExecutionProperty": {
-                "MaxConcurrentRuns": 1
-            },
+            "ExecutionProperty": {"MaxConcurrentRuns": 1},
             "Command": {
                 "Name": "glueetl",
                 "ScriptLocation": S3_BRONZE_USERS_GLUE_SCRIPT_PATH,
@@ -215,5 +235,6 @@ def upload_and_run_aws_glue_job():
 
     upload_task >> run_bronze_posts_glue_job >> run_silver_posts_glue_job
     upload_task >> run_bronze_users_glue_job
+
 
 upload_and_run_aws_glue_job()
